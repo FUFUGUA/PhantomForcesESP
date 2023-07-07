6310ba6fb75ead15cffd4b5e129decdd30522694
Tomi <97225465+FUFUGUA@users.noreply.github.com>
2023-07-07 19:12:05 +0300

    Add files via upload

---
+++ ESPGUI.lua
@@ -0,0 +1,80 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local window = Library.CreateLib("ESP by FUFUGUA (AKA softcare)", "BloodTheme")
+
+local EspTab = window:NewTab("ESP")
+local EspSection = EspTab:NewSection("Esp")
+local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/FUFUGUA/PhantomForcesESP/main/ESPPHANTOMFORCES.lua"))()
+
+ESP.Boxes = false
+ESP.Names = false
+
+local Ghosts = game.Workspace.Players['Bright orange']
+local Phantoms = game.Workspace.Players['Bright blue']
+
+ESP:AddObjectListener(Ghosts, {
+  Recursive = true,
+  Type = "Model",
+  CustomName = " ",
+  Color = Color3.fromRGB(255,165,0),
+      Validator = function(obj)
+      return obj:FindFirstChild("Torso")
+  end,
+  IsEnabled = "GhostsESP"
+})
+ESP:AddObjectListener(Phantoms, {
+  Recursive = true,
+  Type = "Model",
+  CustomName = " ",
+  Color = Color3.fromRGB(0,0,233),
+      Validator = function(obj)
+      return obj:FindFirstChild("Torso")
+  end,
+  IsEnabled = "PhantomsESP"
+})
+ESP.GhostsESP = false
+ESP.PhantomsESP = false 
+
+EspSection:NewToggle("Enable ESP", "Blud what info do you need for this :skull:", function(v)
+    if v then
+        ESP:Toggle(true)
+    else
+        ESP:Toggle(false)
+    end
+end)
+EspSection:NewToggle("Ghosts", "Blud what info do you need for this :skull:", function(v)
+        ESP.GhostsESP = v
+end)
+EspSection:NewToggle("Phantoms", "Blud what info do you need for this :skull:", function(v)
+        ESP.PhantomsESP = v
+end)
+EspSection:NewToggle("Boxes", "Blud what info do you need for this :skull:", function(v)
+    if v then
+        ESP.Boxes = true
+    else
+        ESP.Boxes = false
+    end
+end)
+EspSection:NewToggle("Tracers", "Blud what info do you need for this :skull:", function(v)
+    if v then
+        ESP.Tracers = true
+    else
+        ESP.Tracers = false
+    end
+end)
+EspSection:NewToggle("Distance", "Blud what info do you need for this :skull:", function(v)
+    if v then
+        ESP.Names = true
+    else
+        ESP.names = false
+    end
+end)
+
+EspSection:NewButton("Rejoin", "Rejoins server", function()
+	local ts = game:GetService("TeleportService")
+	local p = game:GetService("Players").LocalPlayer
+	ts:Teleport(game.PlaceId, p)
+end)
+EspSection:NewSlider("FPS capacity", "FPS", 300, 10, function(v)
+	setfpscap(v)
+end)
+
